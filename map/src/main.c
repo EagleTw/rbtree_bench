ebc730c73646a6ed20a95c6de8e96ff2027efcd0
YiPing Pan <pan.yiping.fi@gmail.com>
2023-03-20 19:14:37 +0800

    Apply minibench to codebase

--- map/src/main.c
+++ map/src/main.c
@@ -7,6 +7,7 @@
 
 #include "map.c"
 #include "map.h"
+#include "minibench.h"
 
 void swap(int *x, int *y)
 {
@@ -17,9 +18,10 @@ void swap(int *x, int *y)
 
 enum { NNODES = 1000 * 1000 };
 
+#if 0
 int main()
 {
-    printf("===== Self benchmark: map =====\n");
+    printf("--- Self benchmark: map ---\n");
 
     map_t tree = map_init(int, int, map_cmp_int);
 
@@ -75,3 +77,42 @@ int main()
 
     return 0;
 }
+#endif
+
+BENCHMARK(
+    3,
+
+    map_t tree = map_init(int, int, map_cmp_int);
+
+    int key[NNODES];
+    int val[NNODES];
+
+    /*
+     *  Generate data for insertion
+     */
+    for (int i = 0; i < NNODES; i++) {
+        key[i] = i;
+        val[i] = i;
+    }
+
+    /* This is not a reconmended way to randomize stuff, just a simple test */
+    /* I know MT19937 might be better */
+    srand((unsigned) time(NULL));
+    for (int i = 0; i < 1e7; i++) {
+        int pos_a = rand() % NNODES;
+        int pos_b = rand() % NNODES;
+        swap(&key[pos_a], &key[pos_b]);
+        swap(&val[pos_a], &val[pos_b]);
+    }
+
+    TIMED(
+        "map_insert",
+        for (int i = 0; i < NNODES;
+             i++) { map_insert(tree, key + i, val + i); });
+    TIMED(
+        "map_search", map_iter_t my_it;
+        for (int i = 0; i < NNODES; i++) { map_find(tree, &my_it, key + i); });
+
+    /* There will be a funny behavior with delete */
+//    TIMED("map_delete", map_delete(tree););
+)
